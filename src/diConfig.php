47ef38d26b9d665db981dda2f81e3a2bbd757105
TJ Draper <tj@buzzingpixel.com>
2019-03-19 21:18:43 -0500

    Define the minify interface in the DI so that it could be overridden and swapped out by projects

--- src/diConfig.php
+++ src/diConfig.php
@@ -8,9 +8,14 @@ declare(strict_types=1);
  */
 
 use buzzingpixel\minify\MinifyApi;
+use Psr\Container\ContainerInterface;
+use buzzingpixel\minify\interfaces\MinifyApiInterface;
 
 return [
     MinifyApi::class => function () {
         return new MinifyApi(getenv('DISABLE_MINIFICATION') !== 'true');
+    },
+    MinifyApiInterface::class => function (ContainerInterface $di) {
+        return $di->get(MinifyApi::class);
     }
 ];
